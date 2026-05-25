2dbdcbcb492cf61c669a2b5869e802177fa2474a
dasha2507 <daryna.morykon@oa.edu.ua>
2026-05-25 18:01:50 +0000

    Complete API routes, controllers, and configured SoftDeletes for BlogPost

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
-
+use App\Http\Controllers\Api\Blog\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+    Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
